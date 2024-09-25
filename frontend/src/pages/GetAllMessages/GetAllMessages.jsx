a56431b77511a2ad2c7670632a176d3755067cf1
Dejan Gaspar <107142760+Gaspardious@users.noreply.github.com>
2024-09-25 12:03:59 +0200

    messages showing on frontend

--- frontend/src/pages/GetAllMessages/GetAllMessages.jsx
+++ frontend/src/pages/GetAllMessages/GetAllMessages.jsx
@@ -1,23 +1,50 @@
-import styles from './getallmessages.module.css'
-import React from 'react'
+import { useState, useEffect } from 'react';
+import styles from './getallmessages.module.css';
 
 const GetAllMessages = () => {
+  const [messages, setMessages] = useState([]);
+  const [error, setError] = useState(null);
+
+  // Function to fetch messages
+  const fetchMessages = async () => {
+    try {
+      const response = await fetch('https://wsz0ffu3ta.execute-api.eu-north-1.amazonaws.com/messages');
+      const data = await response.json();
+
+      // Check if data is an array and set messages state accordingly or set error state if data is not an array or fetch fails 
+      if (Array.isArray(data.data)) {
+        setMessages(data.data);
+      } else {
+        setError('Unexpected data format.');
+      }
+    } catch {
+      setError('Failed to load messages.');
+    }
+  };
+
+  useEffect(() => {
+    fetchMessages();
+  }, []);
+
   return (
     <div className={styles.container}>
-
       <h1>Message Board</h1>
-      <ul className={styles.message}>
-        <li className={styles.createdAt}>
-          <p>Created At: </p>
-          <div>
-          <p className={styles.message_text}>Message: </p>
-          </div>
-          <p className={styles.message_username}>Username</p>
-        </li>
-        </ul>
-
+      {error ? <p className={styles.error}>{error}</p> : null} {/* using ternary operator to conditionally render error message if error is not null */}
+      <ul className={styles.messagelist}>
+        {messages.length === 0 ? (
+          <p>No messages found</p>
+        ) : (           // using ternary operator to conditionally render messages if messages array is not empty
+          messages.map(({ messageId, date, message, username }) => (
+            <li key={messageId} className={styles.message}>
+              <p>Created at: {new Date(date).toLocaleString()}</p>
+              <p>Message: {message}</p>
+              <p>Username: {username}</p>
+            </li>
+          ))
+        )}
+      </ul>
     </div>
-  )
-}
+  );
+};
 
-export default GetAllMessages
+export default GetAllMessages;
--- frontend/src/pages/PostNewMessage/PostNewMessage.jsx
+++ frontend/src/pages/PostNewMessage/PostNewMessage.jsx
@@ -1,11 +1,50 @@
 import styles from './postnewmessage.module.css'
+import { useState } from 'react'
 
-import React from 'react'
 
 const PostNewMessage = () => {
+  const [message, setMessage] = useState()
+  const [username, setUsername] = useState()
+
+
+/*   async function fetchMessages() {
+    if (message) {
+      try {
+        const response = await fetch('https://wsz0ffu3ta.execute-api.eu-north-1.amazonaws.com/messages', {
+          method: 'POST',
+          body: JSON.stringify({ message, username }),
+        }
+
+         )
+        const data = await response.json()
+        console.log(data)
+      } catch (error) {
+        console.error(error)
+      }
+    }
+  }
+ */
+
+
+
+
+
+
+
   return (
     <div className={styles.container}>
       <h1>Post New Message</h1>
+      <form>
+        <label for="message">
+          Message:
+          <input type="text" name="message" id="message"   />
+        </label> <br />
+        <label for="username">
+          Username:
+          <input type="text" name="username" id="username" />
+        </label> <br />
+        <button type="submit">Post Message!</button>
+      </form>
 
     </div>
   )
