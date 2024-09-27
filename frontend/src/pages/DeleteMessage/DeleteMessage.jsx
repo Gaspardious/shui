0fc83ee3d2c0e4bd46fda69e94e9c0654a5c42ed
Dejan Gaspar <107142760+Gaspardious@users.noreply.github.com>
2024-09-27 21:02:40 +0200

    Small tweaks

--- frontend/src/pages/DeleteMessage/DeleteMessage.jsx
+++ frontend/src/pages/DeleteMessage/DeleteMessage.jsx
@@ -1,7 +1,7 @@
 import styles from './deletemessage.module.css';
 
 const DeleteMessage = ({ messageId, setMessages, setError }) => {
-  // Function to delete a message
+
   const deleteMessage = async () => {
     try {
       const response = await fetch(
@@ -13,21 +13,21 @@ const DeleteMessage = ({ messageId, setMessages, setError }) => {
           },
         }
       );
-
+  
       if (response.ok) {
-        // Update the state to remove the deleted message
         setMessages((prevMessages) =>
           prevMessages.filter((msg) => msg.messageId !== messageId)
         );
         console.log('Message deleted successfully');
       } else {
         const errorData = await response.json();
-        setError(`Error deleting message: ${errorData.message}`);
+        setError(`Error deleting message: ${errorData.error?.message || 'Unexpected error occurred.'}`);
       }
-    } catch {
+    } catch (error) {
       setError('Failed to delete the message.');
     }
   };
+  
 
   return (
     <button
